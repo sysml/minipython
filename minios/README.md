9f0e6210cd0f1446c485defa60b54923b73098e6
Felipe Huici <felipe.huici@neclab.eu>
2016-05-11 16:48:40 +0200

    minios subdir (i.e., mini-python support)

---
+++ minios/README.md
@@ -0,0 +1,32 @@
+# Mini-python
+
+Mini-python is a Xen virtual machine. It consists of micro-python
+running on top of the Xen paravirtualized MiniOS operating system.
+
+## Building and running
+
+First, edit the Makefile to set the correct paths to your Xen,
+toolchain and MiniOS sources:
+
+    XEN_ROOT                ?= $(realpath ../../[XENSRCS])
+    TOOLCHAIN_ROOT          ?= $(realpath ../../[XENTOOLCHAIN)
+    MINIOS_ROOT             ?= $(realpath ../../[MINIOS])
+
+To build mini-python:
+
+    $ make
+
+To create the VM, first edit mini-python.xen and set the path to the
+VM image:
+
+    kernel = "/path/to/your/vm/image/mini-python_x86_64.gz"
+
+Then create the VM with:
+
+    $ xl create -c mini-python.xen
+
+
+
+
+
+
---
+++ minios/main.c
@@ -0,0 +1,293 @@
+/*
+ * This file is part of the Micro Python project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013, 2014 Damien P. George
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#include "py/mpstate.h"
+#include "py/nlr.h"
+#include "py/compile.h"
+#include "py/runtime.h"
+#include "py/builtin.h"
+#include "py/repl.h"
+#include "py/gc.h"
+#include "py/stackctrl.h"
+#include "py/mphal.h"
+#include "extmod/misc.h"
+#include "genhdr/mpversion.h"
+#include "input.h"
+
+// Command line options, with their defaults
+STATIC bool compile_only = false;
+STATIC uint emit_opt = MP_EMIT_OPT_NONE;
+
+#if MICROPY_ENABLE_GC
+// Heap size of GC heap (if enabled)
+// Make it larger on a 64 bit machine, because pointers are larger.
+long heap_size = 1024*1024 * (sizeof(mp_uint_t) / 4);
+#endif
+
+extern void mp_unix_mark_exec(void);
+
+STATIC void stderr_print_strn(void *env, const char *str, size_t len) {
+    (void)env;
+    ssize_t dummy = write(STDERR_FILENO, str, len);
+    mp_uos_dupterm_tx_strn(str, len);
+    (void)dummy;
+}
+
+const mp_print_t mp_stderr_print = {NULL, stderr_print_strn};
+
+#define FORCED_EXIT (0x100)
+// If exc is SystemExit, return value where FORCED_EXIT bit set,
+// and lower 8 bits are SystemExit value. For all other exceptions,
+// return 1.
+STATIC int handle_uncaught_exception(mp_obj_base_t *exc) {
+    // check for SystemExit
+    if (mp_obj_is_subclass_fast(MP_OBJ_FROM_PTR(exc->type), MP_OBJ_FROM_PTR(&mp_type_SystemExit))) {
+        // None is an exit value of 0; an int is its value; anything else is 1
+        mp_obj_t exit_val = mp_obj_exception_get_value(MP_OBJ_FROM_PTR(exc));
+        mp_int_t val = 0;
+        if (exit_val != mp_const_none && !mp_obj_get_int_maybe(exit_val, &val)) {
+            val = 1;
+        }
+        return FORCED_EXIT | (val & 255);
+    }
+
+    // Report all other exceptions
+    mp_obj_print_exception(&mp_stderr_print, MP_OBJ_FROM_PTR(exc));
+    return 1;
+}
+
+// Returns standard error codes: 0 for success, 1 for all other errors,
+// except if FORCED_EXIT bit is set then script raised SystemExit and the
+// value of the exit is in the lower 8 bits of the return value
+STATIC int execute_from_lexer(mp_lexer_t *lex, mp_parse_input_kind_t input_kind, bool is_repl) {
+    if (lex == NULL) {
+        printf("MemoryError: lexer could not allocate memory\n");
+        return 1;
+    }
+
+    mp_hal_set_interrupt_char(CHAR_CTRL_C);
+
+    nlr_buf_t nlr;
+    if (nlr_push(&nlr) == 0) {
+        qstr source_name = lex->source_name;
+
+        #if MICROPY_PY___FILE__
+        if (input_kind == MP_PARSE_FILE_INPUT) {
+            mp_store_global(MP_QSTR___file__, MP_OBJ_NEW_QSTR(source_name));
+        }
+        #endif
+
+        mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
+
+        /*
+        printf("----------------\n");
+        mp_parse_node_print(parse_tree.root, 0);
+        printf("----------------\n");
+        */
+
+        mp_obj_t module_fun = mp_compile(&parse_tree, source_name, emit_opt, is_repl);
+
+        if (!compile_only) {
+            // execute it
+            mp_call_function_0(module_fun);
+            // check for pending exception
+            if (MP_STATE_VM(mp_pending_exception) != MP_OBJ_NULL) {
+                mp_obj_t obj = MP_STATE_VM(mp_pending_exception);
+                MP_STATE_VM(mp_pending_exception) = MP_OBJ_NULL;
+                nlr_raise(obj);
+            }
+        }
+
+        mp_hal_set_interrupt_char(-1);
+        nlr_pop();
+        return 0;
+
+    } else {
+        // uncaught exception
+        mp_hal_set_interrupt_char(-1);
+        return handle_uncaught_exception(nlr.ret_val);
+    }
+}
+
+#if MICROPY_USE_READLINE == 1
+#include "lib/mp-readline/readline.h"
+#else
+#endif
+
+STATIC int do_str(const char *str) {
+    mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, str, strlen(str), false);
+    return execute_from_lexer(lex, MP_PARSE_FILE_INPUT, false);
+}
+
+STATIC int usage(char **argv) {
+    printf(
+"usage: %s [<opts>] [-X <implopt>] [-c <command>] [<filename>]\n"
+"Options:\n"
+"-v : verbose (trace various operations); can be multiple\n"
+"-O[N] : apply bytecode optimizations of level N\n"
+"\n"
+"Implementation specific options:\n", argv[0]
+);
+    int impl_opts_cnt = 0;
+    printf(
+"  compile-only                 -- parse and compile only\n"
+"  emit={bytecode,native,viper} -- set the default code emitter\n"
+);
+    impl_opts_cnt++;
+#if MICROPY_ENABLE_GC
+    printf(
+"  heapsize=<n> -- set the heap size for the GC (default %ld)\n"
+, heap_size);
+    impl_opts_cnt++;
+#endif
+
+    if (impl_opts_cnt == 0) {
+        printf("  (none)\n");
+    }
+
+    return 1;
+}
+
+// Process options which set interpreter init options
+STATIC void pre_process_options(int argc, char **argv) {
+    for (int a = 1; a < argc; a++) {
+        if (argv[a][0] == '-') {
+            if (strcmp(argv[a], "-X") == 0) {
+                if (a + 1 >= argc) {
+                    exit(usage(argv));
+                }
+                if (0) {
+                } else if (strcmp(argv[a + 1], "compile-only") == 0) {
+                    compile_only = true;
+                } else if (strcmp(argv[a + 1], "emit=bytecode") == 0) {
+                    emit_opt = MP_EMIT_OPT_BYTECODE;
+                } else if (strcmp(argv[a + 1], "emit=native") == 0) {
+                    emit_opt = MP_EMIT_OPT_NATIVE_PYTHON;
+                } else if (strcmp(argv[a + 1], "emit=viper") == 0) {
+                    emit_opt = MP_EMIT_OPT_VIPER;
+#if MICROPY_ENABLE_GC
+                } else if (strncmp(argv[a + 1], "heapsize=", sizeof("heapsize=") - 1) == 0) {
+                    char *end;
+                    heap_size = strtol(argv[a + 1] + sizeof("heapsize=") - 1, &end, 0);
+                    // Don't bring unneeded libc dependencies like tolower()
+                    // If there's 'w' immediately after number, adjust it for
+                    // target word size. Note that it should be *before* size
+                    // suffix like K or M, to avoid confusion with kilowords,
+                    // etc. the size is still in bytes, just can be adjusted
+                    // for word size (taking 32bit as baseline).
+                    bool word_adjust = false;
+                    if ((*end | 0x20) == 'w') {
+                        word_adjust = true;
+                        end++;
+                    }
+                    if ((*end | 0x20) == 'k') {
+                        heap_size *= 1024;
+                    } else if ((*end | 0x20) == 'm') {
+                        heap_size *= 1024 * 1024;
+                    }
+                    if (word_adjust) {
+                        heap_size = heap_size * BYTES_PER_WORD / 4;
+                    }
+#endif
+                } else {
+                    exit(usage(argv));
+                }
+                a++;
+            }
+        }
+    }
+}
+
+#ifdef _WIN32
+#define PATHLIST_SEP_CHAR ';'
+#else
+#define PATHLIST_SEP_CHAR ':'
+#endif
+
+MP_NOINLINE int main_(int argc, char **argv);
+
+int main(int argc, char **argv) {
+    // We should capture stack top ASAP after start, and it should be
+    // captured guaranteedly before any other stack variables are allocated.
+    // For this, actual main (renamed main_) should not be inlined into
+    // this function. main_() itself may have other functions inlined (with
+    // their own stack variables), that's why we need this main/main_ split.
+    mp_stack_ctrl_init();
+    return main_(argc, argv);
+}
+
+MP_NOINLINE int main_(int argc, char **argv) {
+    mp_stack_set_limit(40000 * (BYTES_PER_WORD / 4));
+
+    pre_process_options(argc, argv);
+
+#if MICROPY_ENABLE_GC
+    char *heap = malloc(heap_size);
+    gc_init(heap, heap + heap_size);
+#endif
+
+    mp_init();
+    do_str("print('hello world!')");
+    mp_deinit();
+
+#if MICROPY_ENABLE_GC && !defined(NDEBUG)
+    // We don't really need to free memory since we are about to exit the
+    // process, but doing so helps to find memory leaks.
+    free(heap);
+#endif
+
+    //printf("total bytes = %d\n", m_get_total_bytes_allocated());
+    return 0;
+}
+
+uint mp_import_stat(const char *path) {
+    struct stat st;
+    if (stat(path, &st) == 0) {
+        if (S_ISDIR(st.st_mode)) {
+            return MP_IMPORT_STAT_DIR;
+        } else if (S_ISREG(st.st_mode)) {
+            return MP_IMPORT_STAT_FILE;
+        }
+    }
+    return MP_IMPORT_STAT_NO_EXIST;
+}
+
+void nlr_jump_fail(void *val) {
+    printf("FATAL: uncaught NLR %p\n", val);
+    exit(1);
+}
